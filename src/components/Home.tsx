cf43e5de6347fcc9352dde1cbc7a40308b1999d2
Ludmilla Vaz <ludmillaslvaz@gmail.com>
2025-08-11 18:01:42 -0300

    feat: updating the description and add shadow

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,13 +7,15 @@ export function Home() {
         <img
           src="/lud.svg"
           alt="Foto de Ludmilla Vaz"
-          className="w-32 h-32 rounded-full mx-auto mb-4 border-2 border-background object-cover shadow-[0_0_20px_var(--foreground)]"
+          className="w-32 h-32 rounded-full mx-auto mb-6 border-2 border-background object-cover shadow-[0_0_20px_5px_rgba(168,85,247,0.7)] dark:shadow-[0_0_30px_10px_rgba(124,58,237,0.9)]"
         />
 
-        <h1 className="text-2xl font-bold mb-2">Ludmilla Vaz</h1>
-        <p className="text-[var(--muted-foreground)] leading-relaxed mt-2 font-medium">
-          Desenvolvedora Front-End <br />
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+        <h1 className="text-2xl font-bold mb-1">Ludmilla Vaz</h1>
+        <span className="text-base text-purple-600 dark:text-purple-500 font-medium">
+          Desenvolvedora Front-End
+        </span>
+        <p className="text-white dark:text-black leading-relaxed mt-2 font-light">
+          Desenvolvedora apaixonada por criar experiências digitais que unem design e funcionalidade. Sempre aprendendo, explorando novas tecnologias e buscando transformar ideias em realidade.
         </p>
 
         <div className="flex justify-center gap-4 mt-6">
